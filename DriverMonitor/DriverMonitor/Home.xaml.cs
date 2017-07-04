3bceeacd9a53592e751d795ae35ef74d0e5400be
pancharin <pancharin.b@outlook.com>
2017-07-04 14:45:15 +0700

    tesd

---
+++ DriverMonitor/DriverMonitor/Home.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DriverMonitor
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : ContentPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DriverMonitoring());
+        }
+    }
+}
---
+++ DriverMonitor/DriverMonitor/Maps.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DriverMonitor
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Maps : ContentPage
+    {
+        public Maps()
+        {
+            InitializeComponent();
+        }
+    }
+}
